1a666fee55a2ce642eff6a48b2ea76253739a8fe
zoe-rowe8 <zoemarierowe@hotmail.co.uk>
2023-01-20 15:19:50 +0000

    Adding Endpoints

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ let users = [
     {
         id: 1,
         name: "Kim",
-        favoriteMovies: [Scream]
+        favoriteMovies: []
     },
     {
         id: 2,
         name: "Joe",
-        favoriteMovies: [Ghostbusters]
+        favoriteMovies: []
     },
 ]
 
@@ -171,21 +171,139 @@ let movies = [
     },
   ];
   
-  app.use(express.static("public"));
-  app.use(morgan("common"));
+// CREATE
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send('users need names')
+  }
+})
+
+// UPDATE 
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+let user = users.find( user => user.id == id );
+
+if (user) {
+  user.name = updatedUser.name;
+  res.status(200).json(user);
+} else {
+  res.status(400).send('no such user')
+}
+})
+
+// CREATE 
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+let user = users.find( user => user.id == id );
+
+if (user) {
+  user.favoriteMovies.push(movieTitle);
+  res.status(200).send(`${movieTitle} has been added to user ${id}'s array`); 
+} else {
+  res.status(400).send('no such user')
+}
+})
+
+// DELETE
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+let user = users.find( user => user.id == id );
+
+if (user) {
+  user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+  res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+} else {
+  res.status(400).send('no such user')
+}
+})
+
+// DELETE
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+let user = users.find( user => user.id == id );
+
+if (user) {
+  users = users.filter( user => user.id != id );
+  res.status(200).send(`user ${id} has been deleted`);
+} else {
+  res.status(400).send('no such user')
+}
+})
+
+// READ
+app.get('/movies', (req, res) => {
+  res.status(200).json(movies);
+})
+
+// READ Titles
+app.get('/movies/:title', (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find( movie => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('no such movie')
+  }
+})
+
+// READ Genre
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find( movie => movie.Genre.Name === genreName).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send('no such genre')
+  }
+})
+
+// READ Directors
+app.get('/movies/directors/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find( movie => movie.Director.Name === directorName).Director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send('no such director')
+  }
+})
+
+// listen for requests
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
+
+// Kept just incase, will delete later if not needed?
+
+//  app.use(express.static("public"));
+//  app.use(morgan("common"));
   
   // GET requests
-  app.get("/", (req, res) => {
-    res.send("Welcome to MyFlix!");
-  });
+//  app.get("/", (req, res) => {
+//    res.send("Welcome to MyFlix!");
+//  });
   
   app.get("/documentation", (req, res) => {
     res.sendFile("public/documentation.html", { root: __dirname });
   });
   
-  app.get("/movies", (req, res) => {
-    res.json(topMovies);
-  });
+//  app.get("/movies", (req, res) => {
+//    res.json(topMovies);
+//  });
   
   // Error
   app.use((err, req, res, next) => {
@@ -193,7 +311,3 @@ let movies = [
     res.status(500).send("Something went wrong. Please try again later.");
   });
   
-  // listen for requests
-  app.listen(8080, () => {
-    console.log("Your app is listening on port 8080.");
-  });
