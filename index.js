d9bf05c3719824b0a0bb5fc87c29e4a8dfefcbad
zoe-rowe8 <zoemarierowe@hotmail.co.uk>
2023-02-23 15:50:51 +0000

    Testing Cors Error

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-app.use(cors({
+/* app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
@@ -35,7 +35,9 @@ app.use(cors({
     }
     return callback(null, true);
   }
-}));
+})); */
+
+app.use(cors());
 
 let auth = require('./auth')(app);
 
