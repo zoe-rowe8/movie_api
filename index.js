903345cb0b35b5afc135fb5110ed8b5e2109749e
zoe-rowe8 <zoemarierowe@hotmail.co.uk>
2023-02-24 15:48:29 +0000

    Testing Cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
 /* app.use(cors({
   origin: (origin, callback) => {
